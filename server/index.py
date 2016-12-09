0d824ec04b67e667cc84cd66874f024d109b5ce9
Mikhail Pokhikhilov <dilletante@yandex-team.ru>
2016-12-10 01:03:29 +0300

    Added communication client server

--- server/index.py
+++ server/index.py
@@ -1,3 +1,35 @@
+import websockets
+import asyncio
+import random
+import datetime
+
+
+@asyncio.coroutine
+def hello(websocket, path):
+    name = yield from websocket.recv()
+    print("< {}".format(name))
+
+    greeting = "Hello {}!".format(name)
+    yield from websocket.send(greeting)
+    print("> {}".format(greeting))
+
+@asyncio.coroutine
+def time(websocket, path):
+    while True:
+        now = datetime.datetime.utcnow().isoformat() + 'Z'
+        message = yield from websocket.recv()
+        print("> {}".format(message))
+        yield from websocket.send("Fuck you {}".format(now))
+        # yield from asyncio.sleep(random.random() * 3)
+
 
 if __name__ == "__main__":
-    print("Hello, World")
+    # start_server = websockets.serve(hello, 'localhost', 8765)
+    #
+    # asyncio.get_event_loop().run_until_complete(start_server)
+    # asyncio.get_event_loop().run_forever()
+
+    start_server = websockets.serve(time, '127.0.0.1', 5678)
+
+    asyncio.get_event_loop().run_until_complete(start_server)
+    asyncio.get_event_loop().run_forever()
