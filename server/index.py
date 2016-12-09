cff9363a9bc78f956f880372cacb2189055d422a
Mikhail Pokhikhilov <dilletante@yandex-team.ru>
2016-12-10 01:22:21 +0300

    Fixed some cosmetic issues

--- server/index.py
+++ server/index.py
@@ -1,36 +1,18 @@
 import websockets
 import asyncio
-import random
 import datetime
 
-
-@asyncio.coroutine
-def hello(websocket, path):
-    name = yield from websocket.recv()
-    print("< {}".format(name))
-
-    greeting = "Hello {}!".format(name)
-    yield from websocket.send(greeting)
-    print("> {}".format(greeting))
-
-@asyncio.coroutine
-def time(websocket, path):
+async def time(websocket, path):
     try:
         while True:
             now = datetime.datetime.utcnow().isoformat() + 'Z'
-            message = yield from websocket.recv()
+            message = await websocket.recv()
             print("> {}".format(message))
-            yield from websocket.send("Fuck you {}".format(now))
-            # yield from asyncio.sleep(random.random() * 3)
+            await websocket.send("Fuck you {}".format(now))
     except websockets.ConnectionClosed:
         print("Client closed the connection")
 
 if __name__ == "__main__":
-    # start_server = websockets.serve(hello, 'localhost', 8765)
-    #
-    # asyncio.get_event_loop().run_until_complete(start_server)
-    # asyncio.get_event_loop().run_forever()
-
     start_server = websockets.serve(time, '127.0.0.1', 5678)
 
     asyncio.get_event_loop().run_until_complete(start_server)
