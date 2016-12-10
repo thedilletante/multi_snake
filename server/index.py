7e27b71ebfe4ce790fae9b62ff918e1e0eb0bfd4
Mikhail Pokhikhilov <dilletante@yandex-team.ru>
2016-12-10 07:03:57 +0300

    Added control messages

--- server/index.py
+++ server/index.py
@@ -1,14 +1,47 @@
 import websockets
 import asyncio
 import datetime
-import json
+
+from json import dumps, loads
 from collections import namedtuple
 from enum import Enum
-
 
 Client = namedtuple("Client", ["id", "fd"])
 Position = namedtuple("Position", ["x", "y"])
 
+# protocol
+def greeting(id):
+    return dumps({"id": id})
+
+class InitialMessageBuilder:
+
+    def __init__(self):
+        self.clients = {}
+
+    def add_client(self, id, head, length, direction):
+        self.clients[id] = {
+            "head": {"x": head.x, "y": head.y},
+            "length": length,
+            "direction": str(direction)
+        }
+
+    def build(self):
+        return dumps(self.clients)
+
+
+class PartialStatusMessageBuilder:
+
+    def __init__(self):
+        self.clients = {}
+
+    def add_client_info(self, id, head, direction):
+        self.clients[id] = {
+            "head": {"x": head.x, "y": head.y},
+            "direction": str(direction)
+        }
+
+    def build(self):
+        return dumps(self.clients)
 
 class Direction(Enum):
     top = 0
@@ -29,6 +62,30 @@ class Direction(Enum):
         if self == Direction.bottom:
             return 1
         return 0
+
+    def __str__(self):
+        if self == Direction.top:
+            return "TOP"
+        elif self == Direction.left:
+            return "LEFT"
+        elif self == Direction.bottom:
+            return "BOTTOM"
+        elif self == Direction.right:
+            return "RIGHT"
+        return "UNKNOWN_DIRECTION"
+
+    @staticmethod
+    def create(str_direction):
+        if str_direction == "TOP":
+            return Direction.top
+        elif str_direction == "LEFT":
+            return Direction.left
+        elif str_direction == "BOTTOM":
+            return Direction.bottom
+        elif str_direction == "RIGHT":
+            return Direction.right
+        raise Exception()
+
 
 
 class IntersectionCalculator:
@@ -55,7 +112,7 @@ class Snake:
                         self.body[0].y + self.direction.get_y_factor())
 
     def update(self, direction):
-        self.clients_info[id].direction = direction
+        self.direction = direction
 
 
 class GameBoard:
@@ -82,67 +139,86 @@ class GameBoard:
         for id, client_info in self.clients_info.items():
             info[id] = {"head":{"x":client_info.body[0].x, "y":client_info.body[0].y},
                         "direction":str(client_info.direction)}
-        return json.dumps(info)
+        return dumps(info)
 
 
 class SnakeServer:
+
+    REFRESH_TIMEOUT = 0.5
+    NUM_CLIENTS_TO_GAME = 2
+    BUSY_MESSAGE = "BUSY NOW"
 
     def __init__(self, loop):
         self.clients = []
         self.loop = loop
         self.game_started = False
 
-    async def time(self, websocket, path):
+    async def on_client_connect(self, websocket, path):
+        if self.game_started:
+            websocket.send(SnakeServer.BUSY_MESSAGE)
+            return
+
         id = hash("{}{}".format(websocket.remote_address, datetime.datetime.utcnow().isoformat()))
         client = Client(id, websocket)
         self.clients.append(Client(id, websocket))
 
         try:
-            await websocket.send("You id: {}".format(id))
-            if len(self.clients) == 2:
-                self.game_started = True
+            await websocket.send(greeting(id))
+
+            while not self.game_started:
+                await asyncio.sleep(SnakeServer.REFRESH_TIMEOUT)
+
             while True:
-                now = datetime.datetime.utcnow().isoformat() + 'Z'
                 message = await websocket.recv()
-                print("> {}".format(message))
-                await websocket.send("Fuck you {}".format(now))
+                decoded = loads(message)
+                direction = Direction.create(decoded["direction"])
+                self.board.turn_client(id, direction)
         except websockets.ConnectionClosed:
             self.clients.remove(client)
-            print("Client closed the connection")
 
     async def presentation_loop(self):
-        while not self.game_started:
-            await asyncio.sleep(0.05)
+        while len(self.clients) != 2:
+            await asyncio.sleep(SnakeServer.REFRESH_TIMEOUT)
 
+        self.game_started = True
         clients_info = {}
+        initial_state = InitialMessageBuilder()
         for index, client in enumerate(self.clients):
-            clients_info[client.id] = \
-                Snake(Position(40 + 20 * index, 40 + 20 * index),
-                      20,
-                      Direction.left if index == 0 else Direction.right)
+            head = Position(40 + 20 * index, 40 + 20 * index)
+            length = 20
+            direction = Direction.left if index == 0 else Direction.right
+            clients_info[client.id] = Snake(head, length, direction)
+            initial_state.add_client(client.id, head, length, direction)
 
-        board = GameBoard(100, 100, clients_info)
+        initial_message = initial_state.build()
+
+        for client in self.clients:
+            await client.fd.send(initial_message)
+
+        self.board = GameBoard(100, 100, clients_info)
         num = 0
         while True:
             for client in self.clients:
                 try:
-                    await client.fd.send(board.encode_state())
+                    await client.fd.send(self.board.encode_state())
                 except websockets.ConnectionClosed:
-                    print("Client closed the connection")
                     self.clients.remove(client)
 
             num += 1
-            if num % 50 == 0:
-                board.increase_speed()
+            if num == 50:
+                self.board.increase_speed()
+                num = 0
 
-            board.next()
-            await asyncio.sleep(0.05)
+            self.board.next()
+            await asyncio.sleep(SnakeServer.REFRESH_TIMEOUT)
+
+        self.game_started = False
 
 
 if __name__ == "__main__":
     try:
         server = SnakeServer(asyncio.get_event_loop())
-        start_server = websockets.serve(server.time, '', 5678)
+        start_server = websockets.serve(server.on_client_connect, '', 5678)
 
         asyncio.get_event_loop().run_until_complete(start_server)
         asyncio.get_event_loop().run_until_complete(asyncio.Task(server.presentation_loop()))
--- server/js/simple_sender.js
+++ server/js/simple_sender.js
@@ -9,6 +9,18 @@ ws.onmessage = function (event) {
 };
 document.body.appendChild(messages);
 
-function my_fn() {
-    ws.send("Here I am")
+function my_left() {
+    ws.send('{"direction":"LEFT"}')
+};
+
+function my_up() {
+    ws.send('{"direction":"TOP"}')
+};
+
+function my_bottom() {
+    ws.send('{"direction":"BOTTOM"}')
+};
+
+function my_right() {
+    ws.send('{"direction":"RIGHT"}')
 };
