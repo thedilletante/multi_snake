193470975af82232d868c3360a40feda6697feba
Mikhail Pokhikhilov <dilletante@yandex-team.ru>
2016-12-10 05:32:49 +0300

    Added game board calculation

--- server/index.py
+++ server/index.py
@@ -1,16 +1,103 @@
 import websockets
 import asyncio
 import datetime
+import json
 from collections import namedtuple
 from enum import Enum
 
 Client = namedtuple("Client", ["id", "fd"])
+MoveInfo = namedtuple("MoveInfo", ["head", "direction"])
+
+
+class Direction(Enum):
+    top = 0
+    left = 1
+    bottom = 2
+    right = 3
+
+    def get_x_factor(self):
+        if self == Direction.left:
+            return -1
+        if self == Direction.right:
+            return 1
+        return 0
+
+    def get_y_factor(self):
+        if self == Direction.top:
+            return -1
+        if self == Direction.bottom:
+            return 1
+        return 0
+
+class IntersectionCalculator:
+    def __init__(self):
+        pass
+
+class Position:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def left(self, speed=1):
+        return Position(self.x - speed, self.y)
+
+    def right(self, speed=1):
+        return Position(self.x + speed, self.y)
+
+    def top(self, speed=1):
+        return Position(self.x, self.y - speed)
+
+    def bottom(self, speed=1):
+        return Position(self.x, self.y + speed)
+
+    def move(self, direction, speed=1):
+        if direction == Direction.top:
+            return self.top(speed)
+        elif direction == Direction.left:
+            return self.left(speed)
+        elif direction == Direction.bottom:
+            return self.bottom(speed)
+        elif direction == Direction.right:
+            return self.right(speed)
+        else:
+            raise Exception("Could not move to unknown direction")
+
+class GameBoard:
+
+    def __init__(self, weight, height, clients_info, speed=1):
+        self.weight = weight
+        self.height = height
+        self.clients_info = clients_info
+        self.speed = speed
+
+    def next(self):
+        for id, move_info in self.clients_info.items():
+            new_info = MoveInfo(move_info.head.move(move_info.direction, self.speed),
+                         move_info.direction)
+            self.clients_info[id] = new_info
+
+
+    def turn_client(self, id, direction):
+        if id in self.clients_info:
+            self.clients_info[id].direction = direction
+
+    def increase_speed(self):
+        self.speed *= 2
+
+    def encode_state(self):
+        info = {}
+        for id, client_info in self.clients_info.items():
+            info[id] = {"head":{"x":client_info.head.x, "y":client_info.head.y},
+                        "direction":str(client_info.direction)}
+        return json.dumps(info)
 
 
 class SnakeServer:
 
-    def __init__(self):
+    def __init__(self, loop):
         self.clients = []
+        self.loop = loop
+        self.game_started = False
 
     async def time(self, websocket, path):
         id = hash("{}{}".format(websocket.remote_address, datetime.datetime.utcnow().isoformat()))
@@ -19,6 +106,8 @@ class SnakeServer:
 
         try:
             await websocket.send("You id: {}".format(id))
+            if len(self.clients) == 2:
+                self.game_started = True
             while True:
                 now = datetime.datetime.utcnow().isoformat() + 'Z'
                 message = await websocket.recv()
@@ -29,19 +118,36 @@ class SnakeServer:
             print("Client closed the connection")
 
     async def presentation_loop(self):
+        while not self.game_started:
+            await asyncio.sleep(0.05)
+
+        clients_info = {}
+        for index, client in enumerate(self.clients):
+
+            clients_info[client.id] = \
+                MoveInfo(Position(40 + 20 * index, 40 + 20 * index),
+                         Direction.left if index == 0 else Direction.right)
+        board = GameBoard(100, 100, clients_info)
+        num = 0
         while True:
             for client in self.clients:
                 try:
-                    await client.fd.send("New status is: I don't know")
+                    await client.fd.send(board.encode_state())
                 except websockets.ConnectionClosed:
                     print("Client closed the connection")
                     self.clients.remove(client)
-            await asyncio.sleep(0.5)
+
+            num += 1
+            if num % 50 == 0:
+                board.increase_speed()
+
+            board.next()
+            await asyncio.sleep(0.05)
 
 
 if __name__ == "__main__":
     try:
-        server = SnakeServer()
+        server = SnakeServer(asyncio.get_event_loop())
         start_server = websockets.serve(server.time, '', 5678)
 
         asyncio.get_event_loop().run_until_complete(start_server)
@@ -66,43 +172,6 @@ class Snake:
     def get_new_head_position(self, direction):
         return Position(self.body[0].x + direction.get_x_factor(), self.body[0].y + direction.get_y_factor())
 
-
-class Direction(Enum):
-    top = 0
-    left = 1
-    bottom = 2
-    right = 3
-
-    def get_x_factor(self):
-        if self == Direction.left:
-            return -1
-        if self == Direction.right:
-            return 1
-        return 0
-
-    def get_y_factor(self):
-        if self == Direction.top:
-            return -1
-        if self == Direction.bottom:
-            return 1
-        return 0
-
-class Position:
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
-
-    def left(self):
-        return Position(self.x - 1, self.y)
-
-    def right(self):
-        return Position(self.x + 1, self.y)
-
-    def top(self):
-        return Position(self.x, self.y - 1)
-
-    def bottom(self):
-        return Position(self.x, self.y + 1)
 
 
 class IntersectionCalculator:
