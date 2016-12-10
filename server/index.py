ecf8595e064a1cf4cf01481f811a3ef8ade4e2a5
Mikhail Pokhikhilov <dilletante@yandex-team.ru>
2016-12-10 03:25:54 +0300

    Added class

--- server/index.py
+++ server/index.py
@@ -3,21 +3,27 @@ import asyncio
 import datetime
 import json
 
-async def time(websocket, path):
-    try:
-        id = hash("{}{}".format(websocket.remote_address, datetime.datetime.utcnow().isoformat()))
-        await websocket.send("You id: {}".format(id))
-        while True:
-            now = datetime.datetime.utcnow().isoformat() + 'Z'
-            message = await websocket.recv()
-            print("> {}".format(message))
-            await websocket.send("Fuck you {}".format(now))
-    except websockets.ConnectionClosed:
-        print("Client closed the connection")
+class SnakeServer:
+
+    def __init__(self):
+        self.clients = []
+
+    async def time(self, websocket, path):
+        try:
+            id = hash("{}{}".format(websocket.remote_address, datetime.datetime.utcnow().isoformat()))
+            await websocket.send("You id: {}".format(id))
+            while True:
+                now = datetime.datetime.utcnow().isoformat() + 'Z'
+                message = await websocket.recv()
+                print("> {}".format(message))
+                await websocket.send("Fuck you {}".format(now))
+        except websockets.ConnectionClosed:
+            print("Client closed the connection")
 
 if __name__ == "__main__":
     try:
-        start_server = websockets.serve(time, '', 5678)
+        server = SnakeServer()
+        start_server = websockets.serve(server.time, '', 5678)
 
         asyncio.get_event_loop().run_until_complete(start_server)
         asyncio.get_event_loop().run_forever()
