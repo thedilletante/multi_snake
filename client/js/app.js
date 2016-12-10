fffffeb8d047da8f91252fa56e60571a5d04fac0
Dmitry Korchagin <sildtm@yandex-team.ru>
2016-12-10 16:40:07 +0300

    snake done move

--- client/js/app.js
+++ client/js/app.js
@@ -2,7 +2,11 @@ Math.random() //uncache feature
 // A cross-browser requestAnimationFrame
 // See https://hacks.mozilla.org/2011/08/animating-with-javascript-from-setinterval-to-requestanimationframe/
 var requestAnimFrame = (function(){
-    return window.requestAnimationFrame       ||
+    return function(callback){
+            window.setTimeout(callback, 1000 / 1);
+        };
+
+        window.requestAnimationFrame       ||
         window.webkitRequestAnimationFrame ||
         window.mozRequestAnimationFrame    ||
         window.oRequestAnimationFrame      ||
@@ -16,11 +20,10 @@ var requestAnimFrame = (function(){
 var canvas = document.createElement("canvas");
 var ctx = canvas.getContext("2d");
 var pxPerBox = 10;
-canvas.width = 140 * pxPerBox;
-canvas.height = 75 * pxPerBox;
+canvas.width = 130 * pxPerBox;
+canvas.height = 70 * pxPerBox;
 document.body.appendChild(canvas);
-ctx.fillStyle = '#000000';
-ctx.fillRect(0, 0, canvas.width, canvas.height);
+
 
 var Position = function(x, y) {
     this.x = x;
@@ -28,30 +31,68 @@ var Position = function(x, y) {
 };
 
 var Snake = function(id, headPosition, length, direction) {
+    var renderPosition = function(position) {
+        ctx.rect(position.x*pxPerBox, position.y*pxPerBox, pxPerBox, pxPerBox);
+        var dotMargin = pxPerBox / 3;
+
+        ctx.fillStyle = "white"
+        ctx.fillRect(position.x*pxPerBox + dotMargin, position.y*pxPerBox + dotMargin, pxPerBox - dotMargin*2, pxPerBox - dotMargin*2);
+//        ctx.lineWidth = 2
+//        ctx.strokeStyle = "white"
+//        ctx.stroke();
+    }
     this.body = [headPosition];
     for(i = 1; i < length; i++) {
         lastX = this.body[i - 1].x;
         lastY = this.body[i - 1].y;
         nextPosition = new Position(lastX - getXFactor(direction) , lastY - getYFactor(direction));
         this.body.push(nextPosition);
+        renderPosition(nextPosition)
     }
     this.direction = direction;
     this.id = id;
+    this.update = function(headPosition, direction) {
+        console.log("id:", this.id, "headPosition", headPosition)
+        this.body.unshift(headPosition)
+        this.body.pop()
+        this.body.forEach(function(position) {
+            renderPosition(position)
+        })
+    }
+
+
 };
 
 var snakes = [
-    new Snake("1", new Position(80, 20), 10, 3),
-    new Snake("1", new Position(30, 40), 10, 1)
+    new Snake("1", new Position(90, 20), 10, 3),
+    new Snake("2", new Position(30, 40), 10, 1)
 ];
-render(snakes)
 
 main();
 // The main game loop
 var lastTime;
 function main() {
     handle();
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.fillStyle = '#444444';
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+    snakes.forEach(function(snake) {
+        headPosition = getActualHeadPosition(snake)
+        direction = getActualDirection(snake)
+        snake.update(headPosition, direction)
+    })
+
     requestAnimFrame(main);
 };
+
+function getActualDirection(snake) {
+    return snake.direction
+}
+
+function getActualHeadPosition(snake) {
+    return new Position(snake.body[0].x + getXFactor(snake.direction), snake.body[0].y + getYFactor(snake.direction))
+}
 
 function handle() {
     if(input.isDown('DOWN')) {
@@ -72,21 +113,6 @@ function handle() {
 }
 
 
-
-function render(snakes)  {
-    snakes.forEach(function(snake) {
-        renderSnake(snake);
-    });
-};
-
-function renderSnake(snake) {
-//    console.log(snake)
-    snake.body.forEach(function(position) {
-//        console.log(position)
-       renderPosition(position);
-    });
-}
-
 function getXFactor(direction) {
     if(direction === 1) {
         return -1
@@ -105,19 +131,4 @@ function getYFactor(direction) {
         return 1
     }
     return 0
-}
-
-
-
-function renderPosition(position) {
-    ctx.rect(position.x*pxPerBox, position.y*pxPerBox, pxPerBox, pxPerBox);
-    var dotMargin = pxPerBox / 3;
-    ctx.fillStyle="white";
-    ctx.fillRect(position.x*pxPerBox + dotMargin, position.y*pxPerBox + dotMargin, pxPerBox - dotMargin*2, pxPerBox - dotMargin*2);
-    ctx.lineWidth=2;
-    ctx.strokeStyle="white";
-    ctx.stroke();
-}
-
-function hideBox(box) {
 }
